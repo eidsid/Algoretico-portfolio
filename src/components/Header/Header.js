dd4592ae12dfc877cf5a6b66877fe4dcef5a8f45
eid sid <eidsiddiedis@gmail.com>
2022-03-27 02:00:19 +0200

    working on header add add global styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,37 @@
+import { Link } from "react-router-dom";
+import logo from "assetss/images/logo.png";
 import "./style.scss";
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <Link to="/">
+        <img src={logo} className="logo" alt="Algoretico-logo" />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to="about">About us</Link>
+          </li>
+          <li>
+            <Link to="services">What do we do</Link>
+          </li>
+          <li>
+            <Link to="magazine">Magazine</Link>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/feed/"
+              alt="this linke to opening jobs"
+            >
+              Work with us
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <Link to="contact" className="btn">
+        Let's talk
+      </Link>
+    </header>
+  );
 };
 export default Header;
