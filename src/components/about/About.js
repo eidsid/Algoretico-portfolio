23a191effc131ef54bf803ba6174529a84467685
eid sid <eidsiddiedis@gmail.com>
2022-04-13 01:53:52 +0200

    compelete about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,14 @@
+import Section1 from "./sections/section1/section1";
+import Section2 from "./sections/section2/section2";
+import Section3 from "./sections/section3/section3";
 import "./style.scss";
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className="about">
+      <Section1 />
+      <Section2 />
+      <Section3 />
+    </div>
+  );
 };
 export default About;
