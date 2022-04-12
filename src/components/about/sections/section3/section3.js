257e6fd2486233fd6f918e38624baa2302514ed8
eid sid <eidsiddiedis@gmail.com>
2022-04-13 01:52:43 +0200

    add section3 to about component

---
+++ src/components/about/sections/section3/section3.js
@@ -0,0 +1,27 @@
+import "./style.scss";
+
+const Section3 = () => {
+  return (
+    <div className="a_section3">
+      <div className="left">
+        <p>
+          L’anima di un’Intelligenza Artificiale è fatta di codici. Solo così
+          può funzionare. Algoretico crea IA pensate solo per te e di cui sei il
+          proprietario definitivo. Quell’anima ti appartiene, solo così creerai
+          valore per la tua realtà.
+        </p>
+      </div>
+
+      <div className="right">
+        <p>
+          L’anima di un’Intelligenza Artificiale è fatta di codici. Solo così
+          può funzionare. Algoretico crea IA pensate solo per te e di cui sei il
+          proprietario definitivo. Quell’anima ti appartiene, solo così creerai
+          valore per la tua realtà.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Section3;
