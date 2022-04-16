4b720ae07c82021e7038d15530d3d1ff65338388
eid sid <eidsiddiedis@gmail.com>
2022-04-17 01:50:19 +0200

    create section two in magazine component

--- src/components/magazine/sections/section2/Section2.js
+++ src/components/magazine/sections/section2/Section2.js
@@ -3,7 +3,69 @@ import "./style.scss";
 const Section2 = () => {
   return (
     <div className="M-section2">
-    
+      <div className="col">
+        <div className="left">
+          <h1>non repudation</h1>
+          <p>la Fiducia digtiale</p>
+        </div>
+        <div className="right">
+          <h1>BLOG</h1>
+        </div>
+      </div>
+      <div className="col">
+        <div className="image">
+          <Circel arrowType="right" />
+        </div>{" "}
+        <p>
+          YOUR PLACE your internet Artificial Intelligences are born to solve
+          problems and simplify people's lives.Algoretico makes the AIs you
+          need, and using them is really easy.
+        </p>
+      </div>
+      <div className="col">
+        <p>
+          That they were permitted to do this by the will of Caesar; Further
+          traveling there are certain things which are common. More things are
+          good for me, incline, be loved they wanted to ask. They begged him to
+          use the council of the whole of Gaul for a day on certain occasions.
+          There is the technology you need out there. And we are out there.
+          Artificial Intelligences are able to change the destiny of any la vita
+          delle persone. Algoretico realizza le IA che ti servono, e usarle è
+          davvero semplicissimo. Le Intelligenze Artificiali nascono per
+          risolvere problemi e semplificare la vita delle persone. Algoretico
+          realizza le IA che ti servono, e usarle è davvero semplicissimo. Le
+          Intelligenze Artificialinascono per risolvere problemi e semplificare
+        </p>
+        <p>
+          {" "}
+          projects. This is not science fiction: it is the future that is
+          already happening. L’anima di un’Intelligenza Artificiale è fatta di
+          codici. Solo così può funzionare. Algoretico crea IA pensate solo per
+          te e di cui sei il proprietario definitivo. Quell’anima ti appartiene,
+          solo così creerai valore per la tua realtà. L’anima di un’Intelligenza
+          Artificiale è fatta di codici. Solo così può funzionare. Algoretico
+          crea IA pensate solo per te e di cui sei il proprietario definitivo.
+          Quell’anima ti appartiene, solo così creerai valore per la tua realtà.
+          L’anima di un’Intelligenza Artificiale
+        </p>
+        <p>
+          reality. From classic commercial activities to the most visionary è
+          fatta di codici. Solo così può funzionare. Algoretico crea IA pensate
+          solo per te e di cui sei il proprietario definitivo. Quell’anima ti
+          appartiene, solo così creerai valore per la tua realtà. Le
+          Intelligenze Artificiali nascono per risolvere problemi e semplificare
+        </p>
+        <p>
+          {" "}
+          la vita delle persone. Algoretico realizza le IA che ti servono, e
+          usarle è davvero semplicissimo. sceince of the data What gold mines
+          once were now are data.We deal with Big Data, Data Mining and Data
+          Analysis: this is where the success of each strategy is played. YOUR
+          PLACE your internet Artificial Intelligences are born to solve
+          problems and simplify people's lives.Algoretico makes the AIs you
+          need, and using them is really easy.
+        </p>
+      </div>
     </div>
   );
 };
