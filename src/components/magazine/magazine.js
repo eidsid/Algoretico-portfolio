7fe351f26c62018bb4b690c74c1c21e0c5e785f9
eid sid <eidsiddiedis@gmail.com>
2022-04-18 04:56:58 +0200

    add media query to mixin flex in global styles

--- src/components/magazine/magazine.js
+++ src/components/magazine/magazine.js
@@ -1,13 +1,11 @@
 import Section1 from "./sections/section1/Section1";
 import Section2 from "./sections/section2/Section2";
-import Section3 from "./sections/section3/Section3";
 import "./style.scss";
 const Magazine = () => {
   return (
     <>
       <Section1 />
       <Section2 />
-      <Section3 />
     </>
   );
 };
