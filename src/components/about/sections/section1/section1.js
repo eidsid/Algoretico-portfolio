3f012b8f3597c04af741ed637c44a3d0efe25926
eid sid <eidsiddiedis@gmail.com>
2022-10-08 13:21:30 +0200

    fix layout on smoll screens

--- src/components/about/sections/section1/section1.js
+++ src/components/about/sections/section1/section1.js
@@ -4,27 +4,28 @@ import Circel from "components/shapes/circel/Circel";
 import group11 from "assetss/images/Group11.png";
 
 const Section1 = () => {
+  const background = { backgroundImage: `url(${shafic})` };
   return (
-    <div className="a_section1">
-      <img src={shafic} alt="shafic image" />
-
+    <div className="a_section1" style={background}>
       <div className="container">
         <div className="circel">
           <Circel arrowType="none" />
         </div>
-        <h1>
-          THERE IS A BETTER
-          <span>
-            WAY <span>algoretico</span>
-          </span>
-        </h1>
+        <div>
+          <h1> THERE IS A BETTER</h1>
+          <p>
+            <span>WAY</span> <span>algoretico</span>
+          </p>
+        </div>
 
         <div className="bottom-circel">
           <div className="circle"></div>
           <img src={group11} alt="MEET OUR TEAM icon" />
-          <span>MEET</span>
-          <span>OUR</span>
-          <span>TEAM</span>
+          <div className="textgroup">
+            <span>MEET</span>
+            <span>OUR</span>
+            <span>TEAM</span>
+          </div>
         </div>
       </div>
     </div>
