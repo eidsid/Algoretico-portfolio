3a131a7c3ff3c0b606f822c2191b07fa8ac55cd3
eid sid <eidsiddiedis@gmail.com>
2022-10-08 13:25:37 +0200

    remove unneeded code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Magazine from "components/magazine/magazine";
 import Services from "components/services/Services";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App.scss";
 
 import Header from "./components/Header/Header";
 
